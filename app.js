ebab670a7838da94d43307db20131677b539c56c
Sam <sammeown@gmail.com>
2023-09-26 13:23:22 +0300

    models: added user model + init express app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}...`); // eslint-disable-line no-console
+});
---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const validationMessages = require('../errors/validation');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: [2, validationMessages.minlengthMsg(2)],
+    maxlength: [30, validationMessages.maxlengthMsg(30)],
+    required: [true, validationMessages.requiredMsg()],
+  },
+  email: {
+    type: String,
+    required: [true, validationMessages.requiredMsg()],
+    unique: [true, validationMessages.uniqueMsg()],
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: validationMessages.invalidFormat(),
+    },
+  },
+  password: {
+    type: String,
+    required: [true, validationMessages.requiredMsg()],
+    minlength: [4, validationMessages.minlengthMsg(4)],
+    maxlength: [256, validationMessages.maxlengthMsg(256)],
+    select: false,
+  },
+});
+
+userSchema.set('toObject', {
+  transform: (doc, ret) => {
+    delete ret.__v; // eslint-disable-line no-param-reassign
+    return ret;
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
