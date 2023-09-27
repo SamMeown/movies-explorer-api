56b49531c9766efd0e21726ef19799acc6f97f1f
Sam <sammeown@gmail.com>
2023-09-27 12:59:50 +0300

    errors: added central error processing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const errors = require('./middlewares/errors');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/filmexpdb' } = process.env;
 
@@ -20,6 +21,8 @@ app.post('/signup', createUser);
 app.post('/signin', login);
 
 app.use(auth);
+
+app.use(errors);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}...`); // eslint-disable-line no-console
