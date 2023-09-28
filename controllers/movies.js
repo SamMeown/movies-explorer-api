3f55f8f340109e1fea8b1424d67efdbe62e16a56
Sam <sammeown@gmail.com>
2023-09-28 11:44:28 +0300

    movie routes: added delete-movie handler and route

--- controllers/movies.js
+++ controllers/movies.js
@@ -50,3 +50,30 @@ module.exports.getMovies = (req, res, next) => {
     .then((movies) => res.send(movies.map((movie) => movie.toObject())))
     .catch(next);
 };
+
+module.exports.deleteMovie = (req, res, next) => {
+  const { _id: owner } = req.user;
+  const { movieId } = req.params;
+  Movie.findById(movieId).orFail()
+    .then((movie) => {
+      if (movie.owner.toString() !== owner) {
+        throw new httpErrors.ForbiddenError('Фильм не принадлежит пользователю');
+      }
+
+      return Movie.deleteOne({ _id: movieId, owner }).orFail();
+    })
+    .then(() => {
+      res.send({ message: 'Фильм удален' });
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        next(new httpErrors.NotFoundError('Фильм не найден'));
+        return;
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        next(new httpErrors.BadRequestError(err.message));
+        return;
+      }
+      next(err);
+    });
+};
--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { addMovie, getMovies } = require('../controllers/movies');
+const { addMovie, getMovies, deleteMovie } = require('../controllers/movies');
 
 router.post('/', addMovie);
 router.get('/', getMovies);
+router.delete('/:movieId', deleteMovie);
 
 module.exports = router;
